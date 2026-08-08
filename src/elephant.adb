5e381ded8b815df2d128ec7e0de72059bb900bff
alexmiller63 <46362648+alexmiller63@users.noreply.github.com>
2026-08-07 22:26:24 -0700

    Update elephant.adb

--- src/elephant.adb
+++ src/elephant.adb
@@ -2,9 +2,11 @@ with Ada.Text_IO;
 
 procedure Elephant is
 
-   Memory : String (1 .. 1000);
+   Memory      : String (1 .. 1000);
 
-   Last   : Natural;
+   Last        : Natural;
+
+   Memory_File : Ada.Text_IO.File_Type;
 
 begin
 
@@ -12,7 +14,23 @@ begin
 
    Ada.Text_IO.Get_Line (Memory, Last);
 
-   Ada.Text_IO.Put_Line ("I remember: " & Memory (1 .. Last));
+   Ada.Text_IO.Create
+
+     (File => Memory_File,
+
+      Mode => Ada.Text_IO.Out_File,
+
+      Name => "memories/memory.txt");
+
+   Ada.Text_IO.Put_Line
+
+     (File => Memory_File,
+
+      Item => Memory (1 .. Last));
+
+   Ada.Text_IO.Close (Memory_File);
+
+   Ada.Text_IO.Put_Line ("Memory saved.");
 
 end Elephant;
 
