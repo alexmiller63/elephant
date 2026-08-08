b3cc28345c230e77d51e98edd410f6d9163300a3
alexmiller63 <46362648+alexmiller63@users.noreply.github.com>
2026-08-07 22:21:15 -0700

    Update elephant.adb

--- src/elephant.adb
+++ src/elephant.adb
@@ -1,9 +1,18 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;
 
 procedure Elephant is
 
+   Memory : String (1 .. 1000);
+
+   Last   : Natural;
+
 begin
 
-   Put_Line ("An elephant remembers what matters.");
+   Ada.Text_IO.Put ("Memory: ");
+
+   Ada.Text_IO.Get_Line (Memory, Last);
+
+   Ada.Text_IO.Put_Line ("I remember: " & Memory (1 .. Last));
 
 end Elephant;
+
